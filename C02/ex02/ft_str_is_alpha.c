e7a448b04001ad2105f929915aeedea0743209db
Ricardo Antonio Centeno Cardenas <rcenteno@c3r6s5.42madrid.com>
2024-05-29 11:00:08 +0200

    dia 10

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: rcenteno <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:56:45 by rcenteno          #+#    #+#             */
-/*   Updated: 2024/05/28 16:01:18 by rcenteno         ###   ########.fr       */
+/*   Updated: 2024/05/29 10:53:06 by rcenteno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 
 int	ft_str_is_alpha(char *str)
 {
+	int	index;
 
+	index = 0;
+	while(str[index] != '\n')
+	{
+		if()
+		index++;
+	}
 }
 
 int	main(void)
+{
+	char str[] = 'Hola Mundo';
+	ft_str_is_alpha(str);
+}
